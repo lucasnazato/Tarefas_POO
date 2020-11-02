a6b08b290ff6af25e705d29a9315cb5d8e0ea8bd
lucasnazato <lucas.nazato@hotmail.com>
2020-11-02 11:05:36 -0300

    - + tarefas

---
+++ Tarefa_POO_021120_1/Tarefa_POO_021120_1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Tarefa_POO_021120_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entrada do usuário para definir ordem da matriz
+            Console.Write("Definir ordem da matriz: ");
+            int n = int.Parse(Console.ReadLine());
+
+            // Criar matriz
+            int[,] mat = new int[n, n];
+
+            // Entrada do usuário para preencher a matriz
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("Entrar valor [" + i + ", " + j + "]: ");
+                    mat[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            // Iterar pela matriz para...
+            Console.WriteLine("\nDiagonal Principal: ");
+            int negative = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    // ...Imprimir valores da diagonal e...
+                    if (i == j)
+                    {
+                        Console.Write(mat[i, j] + " ");
+                    }
+
+                    // ...Contar quantos valores negativos estão na matriz
+                    if (mat[i, j] < 0)
+                    {
+                        negative++;
+                    }
+                }
+            }
+
+            // Imprimir quantidade de valores negativos na matriz
+            Console.WriteLine("\nNúmeros Negativos: \n" + negative);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tarefa_POO_021120_2/Tarefa_POO_021120_2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Tarefa_POO_021120_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entrada do usuário para definir tamanho da matriz
+            Console.Write("Número de linhas: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("Número de colunas: ");
+            int m = int.Parse(Console.ReadLine());
+
+            // Criar matriz
+            int[,] mat = new int[n, m];
+
+            // Entrada do usuário para preencher a matriz
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write("Entrar valor [" + i + ", " + j + "]: ");
+                    mat[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            // Imprimir a matriz
+            Console.WriteLine("\nMatriz [" + n + ", " + m + "]");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(mat[i,j].ToString("D2") + " ");
+                }
+                Console.WriteLine();
+            }
+
+            // Pegar valor para procurar na matriz
+            Console.Write("\nNúmero para procura: ");
+            int x = int.Parse(Console.ReadLine());
+
+            // Iterar pela matriz
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    // Se achar o valor imprimir os valores ao redor, desde que estejam dentro da matriz
+                    if (mat[i, j] == x)
+                    {
+                        Console.WriteLine("-Posição [" + i + ", " + j + "]: ");
+                        // Esquerda
+                        if ((j - 1) >= 0) Console.WriteLine("--Esquerda: " + mat[i, j-1]);
+
+                        // Direita
+                        if ((j + 1) < m) Console.WriteLine("--Direita: " + mat[i, j + 1]);
+
+                        // Acima
+                        if ((i - 1) >= 0) Console.WriteLine("--Acima: " + mat[i - 1, j]);
+
+                        // Abaixo
+                        if ((i + 1) < n) Console.WriteLine("--Abaixo: " + mat[i + 1, j]);
+
+                        Console.WriteLine();
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
